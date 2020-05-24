2260add5361bbff5032cc4331ed3dda071555ef2
Josh Goldberg <josh@codecademy.com>
2020-05-24 01:53:29 -0400

    Grr, back to normal imports

--- src/tests/Provider.test.tsx
+++ src/tests/Provider.test.tsx
@@ -1,9 +1,8 @@
+import "mock-redux";
+import { Provider } from "react-redux";
+
 describe("Provider", () => {
   it("throws an error when mock-redux has been imported", async () => {
-    await import("mock-redux");
-
-    const { Provider } = await import("react-redux");
-
     expect(Provider).toThrowError("Provider is not supported when using mock-redux.");
   });
 });
--- src/tests/connect.test.tsx
+++ src/tests/connect.test.tsx
@@ -1,8 +1,8 @@
+import "mock-redux";
+import { connect } from "react-redux";
+
 describe("connect", () => {
   it("throws an error when mock-redux has been imported", async () => {
-    await import("mock-redux");
-    const { connect } = await import("react-redux");
-
     expect(connect).toThrowError("connect is not supported when using mock-redux.");
   });
 });
